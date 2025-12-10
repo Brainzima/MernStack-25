a20b554f5dd603308d1e28d43cf7663863b6de49
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-12-10 09:50:06 +0000

    json formatter is done

--- Lecture-20-JSON-Fromatter/src/App.jsx
+++ Lecture-20-JSON-Fromatter/src/App.jsx
@@ -2,7 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [jsonInput, setJsonInput] = useState("data");
+  const [jsonInput, setJsonInput] = useState("");
+
+  const handleFormat = () => {
+    const parsedData = JSON.parse(jsonInput);
+    const formatedData = JSON.stringify(parsedData, null, 2);
+    setJsonInput(formatedData);
+  }
+
+  const handleMinify = () => {
+    const parsedData = JSON.parse(jsonInput);
+    const formatedData = JSON.stringify(parsedData);
+    setJsonInput(formatedData);
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(jsonInput)
+    alert("Data Copied!");
+  }
 
   const handleClear = () => {
     setJsonInput("")
@@ -14,16 +31,18 @@ function App() {
         <h1 className='text-center'>JSON Formatter</h1>
         <div className="row text-center">
           <div className="col">
-            <button className='btn btn-success m-2'>Format / Beautify</button>
-            <button className='btn btn-primary m-2'>Minify</button>
+            <button onClick={handleFormat} className='btn btn-success m-2'>Format / Beautify</button>
+            <button onClick={handleMinify} className='btn btn-primary m-2'>Minify</button>
             <button className='btn btn-danger m-2' onClick={handleClear}>Clear</button>
+            <button className='btn btn-warning m-2' onClick={handleCopy}>Copy</button>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <textarea name="" id="" rows={30} className='form-control'>
-              {jsonInput}
-            </textarea>
+            <textarea name="" id="" 
+            value={jsonInput} 
+            onChange={(e)=>setJsonInput(e.target.value)} 
+            rows={30} className='form-control' />
           </div>
         </div>
       </div>
