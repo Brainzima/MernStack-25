5ae309e5f88776df70c67c1a86ed0326b4cdfcb1
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-12-15 12:13:04 +0000

    pomodoro done

--- Lecture-22-Pomodoro/src/App.jsx
+++ Lecture-22-Pomodoro/src/App.jsx
@@ -1,21 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [time, setTime] = useState("");
+  const [timeLeft, setTimeLeft] = useState(10);
   const [status, setStatus] = useState(false);
+  const alarm = useRef(new Audio('audio path'))
+
+  useEffect(() => {
+    let interval = null;
+
+    if(status && timeLeft > 0){
+      interval = setInterval(() => {
+        setTimeLeft((time) => time - 1)
+        console.log(timeLeft)
+      }, 1000)
+    }else if(timeLeft == 0){
+      setStatus(false)
+      alarm.current.play();
+      alert("TimeUp")
+    }
+
+    return () => clearInterval(interval) 
+  }, [timeLeft, status])
 
   const handlePomo = () => {
-    setStatus((prev) => !prev);
+    if(status){
+      setStatus(false)
+    }else{
+      setStatus(true)
+    }
+  }
+
+  const formatTime = (seconds) =>{
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    const time = `${min.toString().padStart(2, 0)}:${sec.toString().padStart(2, 0)}`
+    return time;
   }
 
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-        <div className="card bg-warning col-6">
+        <div className="card bg-warning col-8">
           <div className="card-body text-center">
-            <h1 id="time">25:00</h1>
-            <div class="d-grid gap-2">
+            <h1 id="time">{formatTime(timeLeft)}</h1>
+            <div className="d-grid gap-2">
               <button onClick={handlePomo} className={status ? 'btn btn-danger' : 'btn btn-success'}>{status ? 'Stop' : 'Start'}</button>
             </div>
           </div>
