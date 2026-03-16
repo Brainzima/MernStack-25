394459980377506ac6cbba54103d0f57f4c9ed87
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2026-03-16 11:12:43 +0000

    confetti

---
+++ Lecture-49-Confetti/src/App.jsx
@@ -0,0 +1,167 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const { width, height } = useWindowSize()
+  const [chalu, setChalu] = useState(false)
+  const [nop, setNop] = useState(100)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+
+        <input type="number" value={nop} onChange={(e)=>setNop(e.target.value)} />
+        <button className='counter' onClick={()=>setChalu(true)}>
+          Shower On
+        </button>
+        <button className='counter' onClick={()=>setChalu(false)}>
+          Shower Off
+        </button>
+
+        {
+          chalu && <Confetti
+            width={width}
+            height={height}
+            numberOfPieces={nop}
+            gravity={0.5}
+            wind={-0.4}
+          />
+        }
+
+{/* <Confetti
+            width={width}
+            height={height}
+            numberOfPieces={500}
+            gravity={0.5}
+            // run={false}
+            // tweenDuration={10}
+            recycle={false}
+          /> */}
+        {/* <Confetti
+          drawShape={ctx => {
+            ctx.beginPath()
+            for (let i = 0; i < 22; i++) {
+              const angle = 0.35 * i
+              const x = (0.2 + (5.5 * angle)) * Math.cos(angle)
+              const y = (0.2 + (1.5 * angle)) * Math.sin(angle)
+              ctx.lineTo(x, y)
+            }
+            ctx.stroke()
+            ctx.closePath()
+          }}
+        /> */}
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
